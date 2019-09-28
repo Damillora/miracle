ff7ba342bc585d8d0dffc6a444abd304f56ca7eb
Damillora <developer@damillora.com>
2019-09-28 16:45:11 +0700

    Handle not found shorten URL

--- middleware/shorten.js
+++ middleware/shorten.js
@@ -3,6 +3,9 @@ import { tall } from 'tall';
 
 export default function (context) {
   return tall(process.env.SHORTENER_BASE+context.route.path).then(function(unshortenedUrl) {
+    if(unshortenedUrl == process.env.SHORTENER_BASE) {
+      return;
+    }
     context.redirect(unshortenedUrl);
   });
   //  context.redirect("https://nanao.moe");
