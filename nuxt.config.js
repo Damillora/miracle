c607f44c8d1574a9b8450ceebe8caa304f56a5f4
Damillora <developer@damillora.com>
2020-06-24 04:56:09 +0700

    Add Material icons import

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,7 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Exo+2:300,400,500&display=swap' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=M+PLUS+1p:300,400,500&display=swap' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' },
     ],
   },
   /*
