5cd52cfb6252b68540b7d23a220441e162781f9e
Damillora <developer@damillora.com>
2020-07-06 09:54:55 +0700

    Add links

--- pages/links.vue
+++ pages/links.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="page">
-            <h1 class="title">Social Media</h1>
-            <p>I maintain a presence at a number of social media, be sure to follow me there!</p> 
+            <h1 class="title">Links</h1>
+            <p>I maintain a number of profiles on various sites, be sure to follow me there!</p> 
             <p>
               <ul>
                 <li>
@@ -11,6 +11,14 @@
                 <li>
                   <p>Twitter:</p>
                   <p><a href="/twitter">Damillora</a></p>
+                </li>
+                <li>
+                  <p>GitHub:</p>
+                  <p><a href="/github">Damillora</a></p>
+                </li>
+                <li>
+                  <p>Keybase:</p>
+                  <p><a href="/keybase">damillora</a></p>
                 </li>
               </ul>
             </p>
