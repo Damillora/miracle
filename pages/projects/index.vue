9d0aacbba7cb27c3100241933924a91978a19f71
Damillora <developer@damillora.com>
2020-08-05 21:42:53 +0700

    Modify descriptions

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="page">
             <h1 class="title">Project Portfolio</h1>
-            <p>I play arcade and mobile games mostly, not all are recorded / streamed.</p>
-            <p>My most frequently played genre is rhythm games, but sometimes I try other genres too</p>
+            <p>I build applications in my spare time, but currently only some are published / live right now</p>
             <div class="card-list">
-                <ProjectItem name="nanao.moe" codename="miracle" description="The home page for my more playful side." />
+                <ProjectItem name="nanao.moe" codename="miracle" description="The home page for my online identity." />
                 <ProjectItem name="nanao.moe Blog" codename="yuika" description="The blog for my hobbies" />
             </div>
   </div>
