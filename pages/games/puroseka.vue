5f9dab862be07bd9e70681013de547353c8dacd1
Damillora <developer@damillora.com>
2020-11-09 18:22:03 +0700

    Update reviews for CUE! and Project SEKAI

--- pages/games/puroseka.vue
+++ pages/games/puroseka.vue
@@ -2,12 +2,11 @@
   <div class="page">
             <h1 class="title">Game Profile: Project SEKAI COLORFUL STAGE!</h1>
             <p><a href="https://pjsekai.sega.jp/">Game Website</a></p>
-          <!--
             <h2>My Review</h2>
             <blockquote>
+              Overall, I've gotten what I wanted to see in a mobile rhythm game. Virtual Lives are also very interesting with many possibilities that can happen. The other aspects are cherry on top, and they're also not bad too. Project SEKAI is great.
             </blockquote>
-            <p><a href="">Read more</a></p>
-          -->
+            <p><a href="https://blog.nanao.moe/game-reviews/project-sekai-colorful-stage/">Read more</a></p>
   </div>
 </template>
 
