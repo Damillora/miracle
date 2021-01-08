d2afa18c24f296276e53b095d2af750d86dc8955
Damillora <developer@damillora.com>
2021-01-09 03:41:01 +0700

    Add more links

--- pages/links.vue
+++ pages/links.vue
@@ -4,7 +4,8 @@
       <h1>Links</h1>
     </PageHeader>
     <main class="container mx-auto main">
-      <p>I maintain a number of profiles on various sites, be sure to follow me there!</p> 
+      <p>I maintain a number of profiles on various sites, be sure to follow me there!</p>
+      <h2>Common social media</h2>
       <p>
         <ul>
           <li>
@@ -26,6 +27,27 @@
           <li>
             <p>GitHub:</p>
             <p><a href="/github">Damillora</a></p>
+          </li>
+        </ul>
+      </p>
+      <h2>For decentralized web users and the brave</h2>
+      <p>
+        <ul>
+          <li>
+            <p>Fediverse:</p>
+            <p><a href="/fediverse">Damillora@fed.nanao.moe</a></p>
+          </li>
+          <li>
+            <p>Video Channel:</p>
+            <p><a href="/fedichan">damillora@live.nanao.moe</a></p>
+          </li>
+          <li>
+            <p>Matrix</p>
+            <p>@damillora:matrix.nanao.moe</p>
+          </li>
+          <li>
+            <p>Gemini</p>
+            <p><a href="gemini://gemini.nanao.moe">gemini.nanao.moe</a></p>
           </li>
         </ul>
       </p>
