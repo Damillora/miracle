425eea5c6bb2654830fdaff1fd1f0698b91baab5
Damillora <damillora@damillora.com>
2021-01-20 01:44:20 +0700

    Add meishi, GPG public key, and keybase proof

--- pages/about.vue
+++ pages/about.vue
@@ -5,17 +5,26 @@
     </PageHeader>
     <main class="container mx-auto main">
       <p>Hello! My name is Damillora.</p>
-      <p>I enjoy playing rhythm games, and talking about idol and rhythm games in general!</p>
+      <p>I'm currently reconstructing my about page for new things, but I'm a web developer, rhythm game player and music enthusiast!</p>
+      <h2>GPG public key</h2>
+      <p>My public key is accessible <a href="/damillora.asc">here</a>.</p>
+      <h2>Producer Meishi</h2>
+      <div class="gallery">
+        <GalleryImage :src="require('~/assets/images/meishi/front.png')" />
+        <GalleryImage :src="require('~/assets/images/meishi/back.png')" />
+      </div>
     </main>
   </div>
 </template>
 
 <script>
 import PageHeader from '~/components/PageHeader';
+import GalleryImage from '@/components/GalleryImage';
 
 export default {
   components: {
-    PageHeader
+    PageHeader,
+    GalleryImage,
   },
   head: {
     title: 'About Damillora',
